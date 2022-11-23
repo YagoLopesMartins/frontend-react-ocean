d52a25f6475b6a909cac8e7b195c24ec341ed8e3
Yago <yago.lm@samsung.com>
2022-11-23 14:40:41 -0300

    Component Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import "./Header.css"
-import Brand from "../../assets/brand.svg"
+// import Brand from "../../assets/brand.svg"
 
 function Header() {
   return (
     <div className="Header">
       <a href="/">
-        <img src={Brand} width={32} height={32} alt="Samsung Ocean Logo" />
+        {/* <img src={Brand} width={32} height={32} alt="Samsung Ocean Logo" /> */}
       </a>
 
       <a href="/">Listar</a>
